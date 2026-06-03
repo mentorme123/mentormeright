399fbf0b01e147768d7a17b9ae87026be33aaea1
MentorMe Admin <sandeep@mentormeright.in>
2026-06-03 14:00:52 +0530

    Fixed by removing

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,19 +1,3 @@
-const PARTNERS = [
-  { name: "DDGD Vaishnav Chennai", logo: "/images/partners/ddgd-vaishnav.png", link: "/k12-programs" },
-  { name: "Bhavan's Group", logo: "/images/partners/bhavans-group.png", link: "/k12-programs" },
-  { name: "ICBM School of Business", logo: "/images/partners/icbm.png", link: "/college-programs" },
-  { name: "TSWRDC Nalgonda", logo: "/images/partners/tswrdc.png", link: "/k12-programs" },
-  { name: "Pragati Mahavidyalaya", logo: "/images/partners/pragati-maha.png", link: "/college-programs" },
-  { name: "Keshav Memorial", logo: "/images/partners/keshav-memorial.png", link: "/k12-programs" },
-  { name: "ELGI School", logo: "/images/partners/elgi-school.png", link: "/k12-programs" },
-  { name: "St. Joseph's Degree & PG College", logo: "/images/partners/st-josephs.png", link: "/college-programs" },
-  { name: "Howard Park International", logo: "/images/partners/howard-park.png", link: "/k12-programs" },
-  { name: "Geetam School", logo: "/images/partners/geetam.png", link: "/k12-programs" },
-  { name: "Pantheon Digital", logo: "/images/partners/pantheon-digital.png", link: "/college-programs" },
-  { name: "Avanthi Degree & PG Colleges", logo: "/images/partners/avanthi.png", link: "/college-programs" },
-  { name: "HPS Nizamabad", logo: "/images/partners/hps-nizamabad.png", link: "/k12-programs" },
-  { name: "IIMC Hyderabad", logo: "/images/partners/iimc-hyderabad.png", link: "/college-programs" },
-  { name: "St Pious Hyderabad", logo: "/images/partners/st-pious.png", link: "/k12-programs" }
-];
-
-export { PARTNERS };
+export default function Page() {
+  return null;
+}

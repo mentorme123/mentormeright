6ab633052d9ab4c6a476bc2e3ad93848df090305
MentorMe Admin <sandeep@mentormeright.in>
2026-08-22 14:43:43 +0530

    remove curved line divider from blog page header

--- src/app/(main)/blogs/[slug]/page.tsx
+++ src/app/(main)/blogs/[slug]/page.tsx
@@ -95,12 +95,6 @@ export default function BlogPostPage({ params }: { params: { slug: string } }) {
             {post.excerpt}
           </p>
         </div>
-
-        <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20">
-          <svg className="relative block w-[calc(133%+1.3px)] h-[40px] md:h-[80px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.08,130.83,119.22,199.53,108.14Z" className="fill-white"></path>
-          </svg>
-        </div>
       </section>
 
       <section className="relative w-full bg-slate-900">
